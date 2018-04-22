ffc4bfdf4cf32f287a6ef8d93471eb33345faf32
Matthew Talbot <mdtalbot@gmail.com>
2018-04-22 15:55:55 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    res = Rack::Response.new
+    time = Time.now
+    if time.hour < 12
+      res.write("Good Morning!")
+    else
+      res.write("Good Afternoon!")
+    end
+    res.finish
+  end
+end
